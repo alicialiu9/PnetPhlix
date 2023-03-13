4245ad1373e7d8f09f4e1c430b1a9076421d6ff9
alicialiu9 <114616095+alicialiu9@users.noreply.github.com>
2023-03-12 22:44:45 -0700

    Add files via upload

--- treemm.h
+++ treemm.h
@@ -1,7 +1,6 @@
 #ifndef TREEMULTIMAP_INCLUDED
 #define TREEMULTIMAP_INCLUDED
 
-#include <unordered_map>
 #include <vector>
 
 template <typename KeyType, typename ValueType>
@@ -9,48 +8,112 @@ class TreeMultimap {
 public:
     class Iterator {
     public:
-        Iterator() : m_valid(false) {}
-        explicit Iterator(typename std::vector<ValueType>::const_iterator it, typename std::vector<ValueType>::const_iterator end)
-            : m_it(it), m_end(end), m_valid(true) {}
+        Iterator() : m_valid(false), index(0){}
+        Iterator(std::vector<ValueType>* iterator) : m_valid(true), index(0)
+        {
+            m_iterator = iterator;
+        }
 
-        ValueType& get_value() const {  return const_cast<ValueType&>(*m_it);  }
+
+        ValueType& get_value() const
+        {
+        
+            return (*m_iterator)[index];
+        }
         bool is_valid() const { return m_valid; }
-        void advance() {
-            ++m_it;
-            if (m_it == m_end) {
+        void advance()
+        {
+          index++;
+          if  (index >= (*m_iterator).size())
+            {
                 m_valid = false;
             }
         }
-
+    
     private:
-        typename std::vector<ValueType>::const_iterator m_it;
-        typename std::vector<ValueType>::const_iterator m_end;
+        std::vector<ValueType>* m_iterator;
+        int index;
         bool m_valid;
     };
 
-    TreeMultimap() = default;
-    ~TreeMultimap() = default;
-
-    void insert(const KeyType& key, const ValueType& value) {
-        auto it = m_map.find(key);
-        if (it == m_map.end()) {
-            std::vector<ValueType> vec{ value };
-            m_map.emplace(key, std::move(vec));
-        } else {
-            it->second.push_back(value);
-        }
+    TreeMultimap() { m_root = nullptr;}
+    ~TreeMultimap()
+    {
+        // delete all allocate new nodes
     }
 
-    Iterator find(const KeyType& key) const {
-        auto it = m_map.find(key);
-        if (it != m_map.end()) {
-            return Iterator{ it->second.cbegin(), it->second.cend() };
+    void insert(const KeyType& key, const ValueType& value)
+    {
+        if (m_root == nullptr)
+        { m_root = new Node(key, value); return; }
+        
+        Node *cur = m_root;
+        for (;;)
+        {
+            if (key == cur->m_key)
+            {
+                cur->m_values.push_back(value);
+                return; 
+            }
+            if (key < cur->m_key)
+            {
+                if (cur->left != nullptr)
+                    cur = cur->left;
+                else
+                {
+                    cur->left = new Node(key, value);
+                    return;
+                }
+            }
+            else if (key > cur->m_key)
+            {
+                if (cur->right != nullptr)
+                    cur = cur->right;
+                else
+                {
+                    cur->right = new Node(key, value);
+                    return;
+                }
+            }
+            
+        }
+       
+    }
+
+    Iterator find(const KeyType& key) const
+    {
+        Node *ptr = m_root;
+        while (ptr != nullptr)
+        {
+            if (key == ptr->m_key)
+                return Iterator(&(ptr->m_values));
+            else if (key < ptr->m_key)
+                ptr = ptr->left;
+            else
+            ptr = ptr->right;
         }
         return Iterator{};
     }
 
 private:
-    std::unordered_map<KeyType, std::vector<ValueType>> m_map;
+    
+    struct Node
+   {
+       Node(KeyType key,ValueType value)
+       {
+           m_key = key;
+           m_values.push_back(value);
+           left = right = nullptr;
+       }
+
+       KeyType m_key;
+       std::vector<ValueType> m_values;
+       Node *left,*right;
+   };
+    
+    Node *m_root;
+    
+    
 };
 
 
