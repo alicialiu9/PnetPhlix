9d335848c0b29f02818d721651680a391144f3f0
alicialiu9 <114616095+alicialiu9@users.noreply.github.com>
2023-03-13 18:49:17 -0700

    Add files via upload

--- Recommender.cpp
+++ Recommender.cpp
@@ -22,81 +22,24 @@ Recommender::Recommender(const UserDatabase& user_database,
 }
 
 
-bool Recommender::islessthanfunc(MovieAndRank me, MovieAndRank other) const
+bool Recommender::comp(MovietoCompare me, MovietoCompare other)
 {
-    //std::string movie_id;
-   // int compatibility_score;
-    if (me.compatibility_score < other.compatibility_score)
+    if (me.m_rank.compatibility_score > other.m_rank.compatibility_score)
         return true;
-    if (me.compatibility_score == other.compatibility_score)
+    
+    if (me.m_rank.compatibility_score == other.m_rank.compatibility_score)
     {
-        float my_rating = (m_movie_database->get_movie_from_id(me.movie_id))->get_rating();
-        float other_rating = (m_movie_database->get_movie_from_id(other.movie_id))->get_rating();
-        
-        if (my_rating < other_rating)
+        if (me.movie_rating > other.movie_rating)
             return true;
         
-        else if (my_rating == other_rating)
+        else if (me.movie_rating == other.movie_rating)
         {
-            string my_title = (m_movie_database->get_movie_from_id(me.movie_id))->get_title();
-            string other_title = (m_movie_database->get_movie_from_id(other.movie_id))->get_title();
-            
-            return my_title > other_title;
+            return me.movie_title < other.movie_title;
         }
-    
+        
     }
     return false;
 }
-
-void Recommender::merge_sort_MovieRank(vector<MovieAndRank> data, int begin, int end) const
-{
-    if (data.size() <= 1)
-        return;
-    
-   size_t mid = data.size()/2;
-
-    merge_sort_MovieRank(data, begin, mid);
-    merge_sort_MovieRank(data, mid + 1, end);
-   // 5. Merge the two halves using our merge function
-    // n1 = size of left , n2 = size of right
-    merge(data,mid-begin,end-mid);
-
-
-//auto mid = begin + (end - begin) / 2;
-//mergeSort(array, begin, mid);
-//mergeSort(array, mid + 1, end);
-//merge(array, begin, mid, end);
-}
-
-void Recommender::merge (vector<MovieAndRank>& data, int n1, int n2) const
- //(int data[], int n1, int n2, int temp[])
-{
-  int i1=0, i2=0, k=0;
-    
-   vector<MovieAndRank>::iterator A1 = data.begin();
-    vector<MovieAndRank>::iterator A2 = data.begin();
-    for (int i = 0; i<n1;i++)
-    {
-        A2++;
-    }
-   
- vector<MovieAndRank> temp;
-    
-  while (i1 < n1 || i2 < n2)
-  {
-    if (i1 == n1)
-    temp[k++] = A2[i2++];
-    else if (i2 == n2)
-    temp[k++] = A1[i1++];
-    else if (islessthanfunc(data[i1], A2[i2])) //(data[i1] <= A2[i2])
-    temp[k++] = A1[i1++];
-    else
-    temp[k++] = A2[i2++];
-  }
-  for (int i=0;i<n1+n2;i++)
-    data[i] = temp[i];
-}
-
 
 vector<MovieAndRank> Recommender::recommend_movies(const string& user_email, int movie_count) const
 {
@@ -105,12 +48,6 @@ vector<MovieAndRank> Recommender::recommend_movies(const string& user_email, int
     // get user and watch history
     User* u = m_user_database->get_user_from_email(user_email);
     vector<string> watch_hist = u->get_watch_history();
-    
-    cerr << "watch history: " << endl;
-    for (int i = 0 ; i < watch_hist.size(); i++ )
-    {
-        cerr << watch_hist[i] << endl; 
-    }
     
     //iterate through watch history
     for (int i = 0 ; i < watch_hist.size(); i++ )
@@ -183,33 +120,43 @@ vector<MovieAndRank> Recommender::recommend_movies(const string& user_email, int
     std::cerr << "size of candidate map: "  << scores.size()  << std::endl;
     
     // put as MovieAndRank objects
-    vector<MovieAndRank> candidate_sort;
+    vector<MovieAndRank> candidates;
     
     map<string, int>::iterator it = scores.begin();
     while (it != scores.end())
     {
         MovieAndRank temp(it->first, it->second);
-        cerr << it->first << endl;
-        candidate_sort.push_back(temp);
+        candidates.push_back(temp);
         it++;
     }
     
-    std::cerr << "size of movieandrank candidate vector: "  << candidate_sort.size()  << std::endl;
-
-   //sort (candidate_sort.begin(), candidate_sort.end(), islessthanfunc);
+    std::cerr << "size of movieandrank candidate vector: "  << candidates.size()  << std::endl;
     
-    // quicksort 
-    
-    // Movie* c = m_movie_database->get_movie_from_id(m_id);
-    merge_sort_MovieRank(candidate_sort, 0, candidate_sort.size());
-    
-
-    vector<MovieAndRank> recs;
-    for(int i = 0 ; i < movie_count; i++)
+    vector <MovietoCompare> candidates_to_sort;
+    for (int i = 0; i < candidates.size();i++)
     {
-        recs[i] = candidate_sort[i];
+        string id = candidates[i].movie_id;
+        string title = m_movie_database->get_movie_from_id(id)->get_title();
+        float rating = m_movie_database->get_movie_from_id(id)->get_rating();
+        MovietoCompare temp(title, rating, candidates[i]);
+        candidates_to_sort.push_back(temp);
+    }
+
+   sort (candidates_to_sort.begin(), candidates_to_sort.end(), comp);
+
+    
+    int num_recs_to_give = 0;
+    if (movie_count > candidates_to_sort.size())
+        num_recs_to_give = candidates_to_sort.size();
+    else
+        num_recs_to_give = movie_count;
+    
+    vector<MovieAndRank> recs;
+    for(int i = 0 ; i < num_recs_to_give; i++)
+    {
+        recs.push_back(candidates_to_sort[i].m_rank);
     }
     
     return recs;
-    
+
 }
